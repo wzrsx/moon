e202834d3cd6a05535a137e339b3e9fb65e0dbc2
wzrsx <vitgololobov2006@gmail.com>
2025-04-07 12:21:17 +0300

    .

--- script/building.js
+++ script/building.js
@@ -1,22 +1,22 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.SphereGeometry(1, 32, 32);
-var material = new THREE.MeshNormalMaterial();
-var sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
+const material = new THREE.MeshBasicMaterial({ color: 0x808080 }); 
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 camera.position.z = 3;
 
 function render() {
-	requestAnimationFrame( render );
-	sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
-  renderer.render( scene, camera );
+    requestAnimationFrame(render);
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+    renderer.render(scene, camera);
 }
 render();
